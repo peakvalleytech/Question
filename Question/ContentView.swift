eacd4a8b89a117c003411ee3479e8a507d4a2e95
peakvalleytech <peakvalleytech@gmail.com>
2022-10-20 11:45:40 -0700

    Remove unused code

--- Question/ContentView.swift
+++ Question/ContentView.swift
@@ -31,9 +31,7 @@ struct ContentView: View {
                             .font(.title)
                             .fontWeight(.black)
                             .foregroundColor(.primary)
-                    }
-                   
-                    
+                    }  
                 }
                 .frame(maxWidth: UIScreen.main.bounds.size.width - 32, maxHeight:300)
                 .background(Color.mint).cornerRadius(10)
@@ -105,29 +103,7 @@ struct ContentView: View {
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-//            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
